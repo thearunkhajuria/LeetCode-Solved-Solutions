22eab893b4650a918d68d7c7f7bcd1912fee3505
Arun Khajuria <83227294+thearunkhajuria@users.noreply.github.com>
2023-01-02 12:45:30 +0530

    Create Solution.java
    9. Palindrome Number
    Given an integer x, return true if x is a 
    palindrome
    , and false otherwise.
    
     
    
    Example 1:
    
    Input: x = 121
    Output: true
    Explanation: 121 reads as 121 from left to right and from right to left.

---
+++ Easy/9. Palindrome Number/Solution.java
@@ -0,0 +1,21 @@
+// 9. Palindrome Number : https://leetcode.com/problems/palindrome-number/description/
+class Solution {
+    public boolean isPalindrome(int x) {
+        int originalNum = x;
+        int revNum = 0;
+        
+        int num = x;
+        // for negative values
+        if(num<0){
+            return false;
+        }
+        // checking digit of num
+        while(num != 0){
+            int rem = num%10;   //last digit
+            revNum = revNum*10 + rem;   //adding in rev
+            num /= 10;      // reduce the num 
+        }
+        //checking palindrome
+        return originalNum == revNum;
+    }
+}
