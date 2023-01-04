b886fe5e586c0ce1d7c71c09747445e6b3c2fd95
Arun Khajuria <83227294+thearunkhajuria@users.noreply.github.com>
2023-01-04 23:15:04 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (31.49%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,6 +1,18 @@
 class Solution {
+
     public int[] runningSum(int[] nums) {
 
+        // Solution 2: Optimized (and without new array for sum)
+        
+        int[] arr = nums;
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] += arr[i - 1];
+        }
+        return arr; 
+            
+        /*
+        // Solution 1 : Brute-Force
+        
         // copy values
         int[] arr = nums;
         int N = arr.length; // stores length of array
@@ -13,11 +25,11 @@ class Solution {
 
         // assigning other values and finding running sum
         for(int i = 1 ; i < N ; i++){
-      
             runSum[i] = runSum[i-1] + arr[i];
-
         }
-      return runSum;
-
+        
+      return runSum; 
+      */
     }
+
 }
